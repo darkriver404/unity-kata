310970235440536b858bdb357775fe2e0fb29fdc
darkriver404 <darkriver404@outlook.com>
2019-07-01 15:45:21 +0800

    + rename all scenes tool

---
+++ unity-kata/Assets/kata00-tools/RenameTool.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RenameTool
+{
+    /// <summary>
+    /// 重命名场景
+    /// </summary>
+    public static void RenameAllScenes()
+    {
+        //1. find all scenes
+        string[] guids = AssetDatabase.FindAssets("t:Scene");
+
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        foreach (var guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+
+            string dir = Path.GetDirectoryName(path);
+            int pos = dir.LastIndexOf('/');
+            string parentFolder = dir.Substring(pos + 1);
+            Debug.Log(guid + " | " + path + " | " + parentFolder);
+
+            // get index
+            if (dict.ContainsKey(parentFolder))
+            {
+                dict[parentFolder]++;
+            }
+            else
+            {
+                dict.Add(parentFolder, 0);
+            }
+            int index = dict[parentFolder];
+            // get name
+            string newName = (index == 0) ? parentFolder : (parentFolder + "_" + index.ToString());
+            //2. rename
+            AssetDatabase.RenameAsset(path, newName);
+        }
+        dict.Clear();
+        //3. save
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// 重命名资源
+    /// </summary>
+    public static void RenameAsset()
+    {
+        //TODO
+    }
+}
---
+++ unity-kata/Assets/kata00-tools/ToolsEntry.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ToolsEntry : MonoBehaviour
+{
+    [MenuItem("Tools/Rename All Scenes")]
+    static void RenameAllScenes()
+    {
+        RenameTool.RenameAllScenes();
+    }
+
+    [MenuItem("Tools/Rename Asset")]
+    static void RenameAsset()
+    {
+        //TODO
+        //EditorWindow.GetWindow<>
+        RenameTool.RenameAsset();
+    }
+}
